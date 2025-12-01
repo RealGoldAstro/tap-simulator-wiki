c0a98ca8802a58e40d67b114b18c55ea5e394368
Astro <robertbekker2323@gmail.com>
2025-12-01 21:59:35 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -29,7 +29,7 @@ function generateEggHTML(eggName, eggData, worldName) {
     let categoryDisplay;
     if (eggType === "Robux") {
         categoryDisplay = `Robux Store: ${worldName}`;
-    } else if (eggType === "Weekly Leaderboard Pet") {
+    } else if (eggType === "Leaderboard") {
         categoryDisplay = worldName;
     } else {
         categoryDisplay = `World: ${worldName}`;
